a242e8069c301f5575aace741b72e4d4096d7828
HiImGrass <utphuong2005@gmail.com>
2026-08-07 17:04:48 +0700

    Initialize Project

---
+++ src/services/auth.service.js
@@ -0,0 +1,36 @@
+import api from "../api/axios";
+
+const login = async (credentials) => {
+    const response = await api.post("/auth/login", credentials);
+    return response.data;
+};
+
+const register = async (userData) => {
+    const response = await api.post("/auth/register", userData);
+    return response.data;
+};
+
+const refreshToken = async (refreshToken) => {
+    const response = await api.post("/auth/refresh", {
+        refreshToken,
+    });
+    return response.data;
+};
+
+const logout = async () => {
+    const response = await api.post("/auth/logout");
+    return response.data;
+};
+
+const getCurrentUser = async () => {
+    const response = await api.get("/auth/me");
+    return response.data;
+};
+
+export default {
+    login,
+    register,
+    refreshToken,
+    logout,
+    getCurrentUser,
+};
