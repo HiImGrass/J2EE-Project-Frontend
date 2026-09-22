97175e0b9fdf05d7aa6e02ca76136d1812375160
Nguyen An <nguyenan21062005ne@gmail.com>
2026-09-22 10:17:28 +0700

    fix: fix atb currentColor in svg icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import { Icon } from "./ui/icon";
 export function Footer() {
     return (
         <div className='bg-card w-20'>
-            {/* <div className='w-20 h-20 bg-amber-600'>
-                <Icon name="pencil" className="w-6 h-6  text-red-500" />
-            </div> */}
-            FOOTER
+            <div className='w-20 h-20 bg-amber-600 text-red-500'>
+                <Icon name="pencil" className="w-6 h-6 text-red-500" />
+            </div>
+            {/* FOOTER */}
         </div>
     )
 
